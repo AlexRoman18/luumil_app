d0480d8a2fac8c1352169d379063dbaef057efe5
Angel Caamal <angelcamalc@gmail.com>
2026-03-01 23:23:32 -0500

    se arreglo formulario de la solicitud para comerciante

--- lib/screens/comer/registro_screen.dart
+++ lib/screens/comer/registro_screen.dart
@@ -8,6 +8,7 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         fit: StackFit.expand,
         children: [
